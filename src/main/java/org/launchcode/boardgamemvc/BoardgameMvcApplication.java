2c4a32b7fda6342c3969fe2ca26bd24b0d307bd1
Michelle Hilmer <mblanke1988@gmail.com>
2019-10-24 22:46:16 -0500

    first commit

---
+++ src/main/java/org/launchcode/boardgamemvc/BoardgameMvcApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.boardgamemvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardgameMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardgameMvcApplication.class, args);
+	}
+
+}
